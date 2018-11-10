50a878b9164bad42810285db6f0cd6d8b2d5ea82
Ashish Gupta <ashish123456.gupta@gmail.com>
2018-11-10 18:16:54 +0530

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,3 +1,5 @@
+Dijkstra with Path:
+
 int arrival[N], departure[N], vis[N], parent[N];
 vector<pair<int, int> > g[N];
 
@@ -45,6 +47,39 @@ void dijkstra(int source, int destination)
 	reverse(ans.begin(), ans.end());
 	for(auto it:ans)
 		cout<<it<<" ";
+}
+
+-------------------------------------------------------------------------------------------------------------------------------------
+Simple Dijkstra:
+
+int arrival[N][N], departure[N][N];
+vector<pair<int, int> > g[N];
+
+void dijkstra(int source, int destination, int arrival[], int departure[])
+{
+	for(int i=1;i<=n;i++)
+	{
+		arrival[i]=inf;
+		departure[i]=inf;
+	}
+	arrival[source]=0;
+	set<pair<int, int> > s;
+	s.insert({0, source});
+	while(!s.empty())
+	{
+		auto x = *(s.begin());
+		s.erase(x);
+		departure[x.second]=arrival[x.second];
+		for(auto it:g[x.second])
+		{
+			if(arrival[it.first] > departure[x.second] + it.second)
+			{
+				s.erase({arrival[it.first], it.first});
+				arrival[it.first]=departure[x.second] + it.second;
+				s.insert({arrival[it.first], it.first});
+			}
+		}
+	}
 }
 
 //Sample Problem 1 (Direct Dijkstra): https://codeforces.com/contest/20/problem/C
