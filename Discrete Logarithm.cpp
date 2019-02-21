388fb65fa6c67c2fa8b000cfe251e4144fb23a9e
Ashish Gupta <ashish123456.gupta@gmail.com>
2019-02-21 14:12:30 +0530

    Rename Discrete Logarithm to Discrete Logarithm.cpp

---
+++ Discrete Logarithm.cpp
@@ -0,0 +1,54 @@
+//Finds smallest x such that a^x mod m = b mod m, returns -1 if there is no such x
+//Complexity: O(sqrtM * logM)
+
+long long pow(long long a, long long b, long long m)
+{
+	long long ans=1;
+	while(b)
+	{
+		if(b&1)
+			ans=(ans*a)%m;
+		b/=2;
+		a=(a*a)%m;
+	}
+	return ans;
+}
+
+int discreteLog(int a, int b, int m)
+{
+	a %= m, b %= m;
+	if(b == 1)
+		return 0;
+	int cnt = 0;
+	long long t = 1;
+	for(int curg=__gcd(a, m);curg!=1;curg=__gcd(a, m))
+	{
+		if(b % curg)
+			return -1;
+		b /= curg, m /= curg, t = (t * a / curg) % m;
+		cnt++;
+		if(b == t)
+			return cnt;
+	}
+
+	map<int, int> hash;	
+	int mid = ((int)sqrt(1.0 * m) + 1);
+	long long base = b;
+	for(int i=0;i<mid;i++)
+	{
+		hash[base] = i;
+		base = base * a % m;
+	}
+
+	base = pow(a, mid, m);
+	long long cur = t;
+	for(int i=1;i<=mid+1;i++)
+	{
+		cur = cur * base % m;
+		if(hash.count(cur))
+			return i * mid - hash[cur] + cnt;
+	}
+}
+
+//Problem 1: https://codeforces.com/gym/101853/problem/G
+//Solution 1: https://codeforces.com/gym/101853/submission/50273547
