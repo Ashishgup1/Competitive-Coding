c10b484579026e37df4eb4145303aa57c85ed9a5
Ashish Gupta <ashish123456.gupta@gmail.com>
2019-02-24 14:55:31 +0530

    Create Persistent Segment Tree.cpp

---
+++ Persistent Segment Tree.cpp
@@ -0,0 +1,58 @@
+int n, m, ct=0, num=0;
+int root[N], st[21*N], lc[21*N], rc[21*N];
+
+int build(int L, int R)
+{
+	int node = ++ct;
+	if(L==R)
+		return node;
+	int M = (L+R)/2;
+	lc[node] = build(L, M);
+	rc[node] = build(M+1, R);
+	return node;
+}
+
+int update(int onode, int L, int R, int pos)
+{
+	int node = ++ct;
+	if(L==R)
+	{
+		st[node] = st[onode] + 1;
+		return node;
+	}
+	int M = (L+R)/2;
+	lc[node] = lc[onode];
+	rc[node] = rc[onode];
+	if(pos <= M)
+		lc[node] = update(lc[onode], L, M, pos);
+	else
+		rc[node] = update(rc[onode], M+1, R, pos);
+	st[node] = st[lc[node]] + st[rc[node]];
+	return node;
+}
+
+int query(int nodeu, int nodev, int L, int R, int pos)
+{
+	if(L==R)
+		return L;
+	int M = (L+R)/2;
+	int leftval = st[lc[nodev]] - st[lc[nodeu]];
+	int rightval = st[rc[nodev]] - st[rc[nodeu]];
+	if(leftval>=pos)
+		return query(lc[nodeu], lc[nodev], L, M, pos);
+	else
+		return query(rc[nodeu], rc[nodev], M+1, R, pos - leftval);
+}
+
+void persistentSegTree()
+{
+	root[0] = build(1, num);
+	for(int i=1;i<=n;i++)
+		root[i] = update(root[i-1], 1, num, a[i]);
+}
+
+//Problem 1 (kth minimum weight on the path from node u to node v): https://www.spoj.com/problems/COT/
+//Solution 1: http://p.ip.fi/KLiG
+
+//Problem 2 (Kth element in sorted range [i, j]): https://www.spoj.com/problems/MKTHNUM/
+//Solution 2: http://p.ip.fi/B00r
