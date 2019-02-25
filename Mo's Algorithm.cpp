4ac2ffc66c0c30abc9d476f593d50a351115fb82
Ashish Gupta <ashish123456.gupta@gmail.com>
2019-02-25 23:00:57 +0530

    Update Mo's Algorithm.cpp

--- Mo's Algorithm.cpp
+++ Mo's Algorithm.cpp
@@ -1,87 +1,60 @@
-const int N=2e5+5;
-const int M=1e6+5;
+const int N = 2e5 + 5;
+const int Q = 2e5 + 5;
+const int M = 1e6 + 5;
+const int SZ = sqrt(N) + 1;
 
 struct data
 {
-	int l;
-	int r;
-	int idx;
-	long long store_ans;
-};
+	int l, r, idx;
+}qr[Q];
 
-int n, q, blocksz=1000;
-int a[N];
-data queries[N];
-long long freq[M];
-long long ans=0;
+int n, q, a[N];
+int freq[M];
+long long ans[N];
+long long cur = 0;
 
-bool comp(data &d1, data &d2)
+bool comp(struct data &d1, struct data &d2)
 {
-	int blocka=d1.l/blocksz;
-	int blockb=d2.l/blocksz;
-	if(blocka<blockb)
-		return true;
-	else if(blocka==blockb)
-		return (d1.r<d2.r)^(blocka%2);
-	else  
-		return false;
-}
-
-bool comp2(data &d1, data &d2)
-{
-	return d1.idx<d2.idx;
-}
-
-void update(long long k, int sign) //Sign 1 = Add, -1 = Remove
-{
-	if(sign==1)
-	{
-		ans-=freq[k]*freq[k]*k;
-		freq[k]++;
-		ans+=freq[k]*freq[k]*k;
-	}
+	int b1 = d1.l / SZ;
+	int b2 = d2.l / SZ;
+	if(b1 != b2)
+		return b1 < b2;
 	else
-	{
-		ans-=freq[k]*freq[k]*k;
-		freq[k]--;
-		ans+=freq[k]*freq[k]*k;
-	}
+		return (b1 & 1) ? d1.r < d2.r : d1.r > d2.r;
 }
 
-void calcmo()
+inline void add(int x)
+{	
+	cur -= 1LL * freq[x] * freq[x] * x;
+	freq[x]++;
+	cur += 1LL * freq[x] * freq[x] * x;
+}
+
+inline void remove(int x)
 {
-	int moleft=1;
-	int moright=0;
+	cur -= 1LL * freq[x] * freq[x] * x;
+	freq[x]--;
+	cur += 1LL * freq[x] * freq[x] * x;
+}
+
+void mo()
+{
+	sort(qr + 1, qr + q + 1, comp);
+	int l = 1, r = 0;
+	cur = 0;
 	for(int i=1;i<=q;i++)
 	{
-		int r=queries[i].r;
-		int l=queries[i].l;
-		while(moright<r)
-		{
-			moright++;
-			update(a[moright], 1);
-		}
-		while(moright>r)
-		{
-			update(a[moright], -1);
-			moright--;
-		}
-		while(moleft<l)
-		{
-			update(a[moleft], -1);
-			moleft++;
-		}
-		while(moleft>l)
-		{
-			moleft--;
-			update(a[moleft], 1);
-		}
-		queries[i].store_ans=ans;
+		while(l < qr[i].l) remove(a[l++]);
+		while(l > qr[i].l) add(a[--l]);
+		while(r < qr[i].r) add(a[++r]);
+		while(r > qr[i].r) remove(a[r--]);
+		ans[qr[i].idx] = cur;
 	}
 }
 
+
 //Problem 1: https://codeforces.com/contest/86/problem/D
-//Solution 1: https://codeforces.com/contest/86/submission/45491857
+//Solution 1: https://codeforces.com/contest/86/submission/50501513
 
 //Problem 2 (Prefix Xor): https://codeforces.com/contest/617/problem/E
 //Solution 2: https://codeforces.com/contest/617/submission/45491963
