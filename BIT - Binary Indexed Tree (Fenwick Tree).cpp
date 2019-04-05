6f61ebc260bd74fcadb6a992e6bf705cdf4ff0db
Ashish Gupta <ashish123456.gupta@gmail.com>
2019-04-05 18:15:42 +0530

    Update BIT - Binary Indexed Tree (Fenwick Tree).cpp

--- BIT - Binary Indexed Tree (Fenwick Tree).cpp
+++ BIT - Binary Indexed Tree (Fenwick Tree).cpp
@@ -27,6 +27,65 @@ int rsum(int l, int r)
 	return pref(r) - pref(l-1);
 }
 
+----------------------------------------------------------------------------------------------------------------------
+
+struct BIT
+{
+	int N;
+	vector<int> bit;
+
+	void init(int n)
+	{
+		N = n;
+		bit.assign(n + 1, 0);
+	}
+
+	void update(int idx, int val)
+	{
+		while(idx <= N)
+		{
+			bit[idx] += val;
+			idx += idx & -idx;
+		}
+	}
+
+	void updateMax(int idx, int val)
+	{
+		while(idx <= N)
+		{
+			bit[idx] = max(bit[idx], val);
+			idx += idx & -idx;
+		}
+	}
+
+	int pref(int idx)
+	{
+		int ans = 0;
+		while(idx > 0)
+		{
+			ans += bit[idx];
+			idx -= idx & -idx;
+		}
+		return ans;
+	}
+
+	int rsum(int l, int r)
+	{
+		return pref(r) - pref(l - 1);
+	}
+
+	int prefMax(int idx)
+	{
+		int ans = -2e9;
+		while(idx > 0)
+		{
+			ans = max(ans, bit[idx]);
+			idx -= idx & -idx;
+		}
+		return ans;
+	}
+};
+
 Multiple BIT:
 
 int bit[2][N];
