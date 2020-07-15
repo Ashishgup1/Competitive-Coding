9a4cc5035bc8a1524837b365a7c5898882db226a
Ashish Gupta <ashish123456.gupta@gmail.com>
2020-07-15 16:11:44 +0530

    Update Topological Sort.cpp

--- Topological Sort.cpp
+++ Topological Sort.cpp
@@ -1,34 +1,30 @@
+vector<int> order; //Stores the Topological Order
 
-int indeg[N];
-vector<int> topo; //Stores lexicographically smallest toposort
-vector<int> g[N];
-
-bool toposort() //Returns 1 if there exists a toposort, 0 if there is a cycle
+bool toposort(int n) //Returns 1 if there exists a toposort, 0 if there is a cycle
 {	
-	priority_queue<int, vector<int>, greater<int> > pq;
-	for(int i=1;i<=n;i++)
+	queue<int> q;
+	vector<int> indeg(n + 1, 0);
+	for(int i = 1; i <= n; i++)
 		for(auto &it:g[i])
 			indeg[it]++;
-	for(int i=1;i<=n;i++)
+	for(int i = 1; i <= n; i++)
 	{
 		if(!indeg[i])
-			pq.push(i);
+			q.push(i);
 	}
-	while(!pq.empty())
+	while(!q.empty())
 	{
-		int u=pq.top();
-		pq.pop();
-		topo.push_back(u);
+		int u = q.front();
+		q.pop();
+		order.push_back(u);
 		for(auto &v:g[u])
 		{
 			indeg[v]--;
 			if(!indeg[v])
-				pq.push(v);
+				q.push(v);
 		}
 	}
-	if(topo.size()<n)
-		return 0;
-	return 1;
+	return (order.size() == n);
 }
 
 //Problem 1: https://www.spoj.com/problems/TOPOSORT/
